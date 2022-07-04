e90360d953d8ee995b11a69ad20f31e79dcf1f01
AnthonyG98 <gonz.anthony6@gmail.com>
2022-07-03 22:24:25 -0700

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,13 @@
 import React from "react";
-
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store/auth-slice";
 function Login() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const handleLogin = () => {
+    dispatch(authActions.login());
+  };
+  console.log(isLoggedIn);
   return (
     <div className="form-container">
       <div className="inputs">
@@ -17,7 +24,10 @@ function Login() {
           <label>Password</label>
           <input type="password" />
         </div>
-        <button className="form-btn">LOGIN</button>
+        <button className="form-btn" onClick={handleLogin}>
+          LOGIN
+        </button>
+        <p>New user? Sign up here</p>
       </div>
     </div>
   );
